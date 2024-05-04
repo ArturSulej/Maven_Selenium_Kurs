c812d325260941e8266cbecc08f91007bcdd7af6
bluescoob <sulejartur2@gmail.com>
2024-05-04 11:36:17 +0200

    Dodanie WebDriver dla IE, Chrome i Firefox

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,9 @@
 package org.example;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 public class Main {
     public static void main(String[] args) {
 
-        WebDriver driver = new FirefoxDriver();
+        System.out.println("Hello world!");
     }
 
 }
--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,9 +1,46 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
+    public WebDriver getDriver(String browser){
+        return switch (browser) {
+            case "chrome" -> new ChromeDriver();
+            case "firefox" -> new FirefoxDriver();
+            case "ie" -> new InternetExplorerDriver();
+            default -> throw new IllegalArgumentException("Invalid browser name");
+        };
+    }
+
     @Test
-    public void sampleTest(){
-        System.out.println("Test");
+    public void openGooglePageChrome(){
+        // Poniższe kroki są opcjonalne
+        /*
+        String path = "C:\\selenium-java-4.20.0\\selenium-chrome-driver-4.20.0.jar";
+        System.setProperty("webdriver.chrome.driver",path);
+
+        // Otwarcie przeglądarki Chrome
+        ChromeDriver driver = new ChromeDriver();
+        // Otwarcie strony
+        driver.get("https://www.google.com");
+         */
+        WebDriver driver = getDriver("chrome");
+        driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void openGooglePageFirefox(){
+        WebDriver driver = getDriver("firefox");
+        driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void openGooglePageIE(){
+        WebDriver driver = getDriver("ie");
+        driver.get("https://www.google.com");
     }
 }
