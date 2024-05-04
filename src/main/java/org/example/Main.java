026d4511c563489a856d6e7d0ca49874263b221b
bluescoob <sulejartur2@gmail.com>
2024-05-04 10:49:43 +0200

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        WebDriver driver = new FirefoxDriver();
     }
 }
