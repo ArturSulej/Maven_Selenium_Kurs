7c17794faaac2b8a81e0e4f5a8527918462405cd
bluescoob <sulejartur2@gmail.com>
2024-05-05 17:26:43 +0200

    Dodanie DataProvider

--- src/test/java/testy_ng/DependencyTests.java
+++ src/test/java/testy_ng/DependencyTests.java
@@ -1,5 +1,6 @@
 package testy_ng;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DependencyTests {
@@ -17,5 +18,18 @@ public class DependencyTests {
     @Test
     public void thirdTest(){
         System.out.println("I am third test");
+    }
+
+    @Test(dataProvider = "data")
+    public void dpTest(String value){
+        System.out.println(value);
+    }
+
+    @DataProvider(name = "data")
+    public Object[][] dataProvider(){
+        return new Object[][] {{"I am first test"},
+                {"I am second test"},
+                {"I am third test"}
+        };
     }
 }
